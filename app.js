a3f0b0b117b5c5e12b2e0e31a7412e9ba80e275d
1001hadi <hadi.jaf.1001@gmail.com>
2023-04-10 10:20:27 -0600

    Day of the weeks updated

--- app.js
+++ app.js
@@ -1,49 +1,69 @@
-const clock = document.getElementById('clock');
-const date = document.getElementById('date');
+const clock = document.getElementById("clock");
+const date = document.getElementById("date");
 
 function timeGenerator() {
-    const curTime = new Date();
-    let curHour = curTime.getHours();
-    let curMinute = curTime.getMinutes();
-    let curSecond = curTime.getSeconds();
-    let amPmConvertor = (curHour >= 12) ? 'PM' : 'AM';
+  const curTime = new Date();
+  let curHour = curTime.getHours();
+  let curMinute = curTime.getMinutes();
+  let curSecond = curTime.getSeconds();
+  let amPmConvertor = curHour >= 12 ? "PM" : "AM";
 
+  if (curHour > 12) {
+    curHour = curHour - 12;
+  } else if (curHour <= 9) {
+    curHour = "0" + curHour;
+  } else {
+    curHour;
+  }
 
-    if(curHour > 12) {
-        curHour = curHour -12;
-    } else if (curHour <= 9) {
-        curHour = '0' + curHour;
-    } else {
-        curHour;
-    }
+  if (curMinute <= 9) {
+    curMinute = "0" + curMinute;
+  } else {
+    curMinute;
+  }
 
-    if (curMinute <= 9) {
-        curMinute = '0' + curMinute;
-    } else {
-        curMinute;
-    }
+  if (curSecond <= 9) {
+    curSecond = "0" + curSecond;
+  } else {
+    curSecond;
+  }
 
-    if (curSecond <= 9) {
-        curSecond = '0' + curSecond;
-    } else {
-        curSecond;
-    }
-
-    clock.innerText = `${curHour}:${curMinute}:${curSecond} ${amPmConvertor}`;
+  clock.innerText = `${curHour}:${curMinute}:${curSecond} ${amPmConvertor}`;
 }
 
- const months = ["January","February","March","April","May","June","July", "August","September","October","November","December"]; 
- const daysOfWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday"];
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+const daysOfWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "saturday",
+];
 
 function dateGenerator() {
-    const exactDate = new Date();
-    const curYear = exactDate.getFullYear();
-    const curMonth = months[exactDate.getMonth()];
-    const curDate = exactDate.getDate();
-    const curDay = daysOfWeek[exactDate.getDay()];
+  const exactDate = new Date();
+  const curYear = exactDate.getFullYear();
+  const curMonth = months[exactDate.getMonth()];
+  const curDate = exactDate.getDate();
+  const curDay = daysOfWeek[exactDate.getDay()];
 
-    date.innerText = `${curDay}, ${curMonth} ${curDate} ${curYear}`;
+  date.innerText = `${curDay}, ${curMonth} ${curDate} ${curYear}`;
 }
 
-setInterval(timeGenerator,0);
+setInterval(timeGenerator, 0);
 dateGenerator();
